0ce251e17b68c51936d7f0a45dd39f248b8cd55c
DmitriiUB <dmitrii.ml@mail.ru>
2022-07-11 18:57:10 +0700

    <Requred>

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,19 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Proba();
+
+
+        }
+
+        private static void Proba()
+        {
+            var world=Console.ReadLine();
+            Console.WriteLine(world);
+            Console.WriteLine(world);
+            Console.WriteLine(world);
+            Console.WriteLine(world);
+            Console.WriteLine(world);
         }
     }
 }
